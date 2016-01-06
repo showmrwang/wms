4e8adb5314695a718b48aee7454422b9bb56898c
bin.hu <bin.hu@baozun.com>
2016-01-06 15:42:50 +0800

    po asn

---
+++ warehouse-operation-impl/src/main/java/com/baozun/scm/primservice/whoperation/dao/poasn/WhPoDao.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2013 Baozun All Rights Reserved.
+ * 
+ * This software is the confidential and proprietary information of Baozun. You shall not disclose
+ * such Confidential Information and shall use it only in accordance with the terms of the license
+ * agreement you entered into with Baozun.
+ * 
+ * BAOZUN MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF THE SOFTWARE, EITHER
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE, OR NON-INFRINGEMENT. BAOZUN SHALL NOT BE LIABLE FOR ANY DAMAGES
+ * SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS
+ * DERIVATIVES.
+ * 
+ */
+package com.baozun.scm.primservice.whoperation.dao.poasn;
+
+import java.util.Map;
+
+import lark.common.annotation.CommonQuery;
+import lark.common.annotation.QueryPage;
+import lark.common.dao.Page;
+import lark.common.dao.Pagination;
+import lark.common.dao.Sort;
+import lark.orm.dao.supports.BaseDao;
+
+import com.baozun.scm.primservice.whoperation.model.poasn.WhPo;
+
+
+
+public interface WhPoDao extends BaseDao<WhPo, Long> {
+
+    @QueryPage("findListCountByQueryMap")
+    Pagination<WhPo> findListByQueryMapWithPage(Page page, Sort[] sorts, Map<String, Object> params);
+
+    @CommonQuery
+    int saveOrUpdate(WhPo o);
+
+    @CommonQuery
+    int saveOrUpdateByVersion(WhPo o);
+
+}
