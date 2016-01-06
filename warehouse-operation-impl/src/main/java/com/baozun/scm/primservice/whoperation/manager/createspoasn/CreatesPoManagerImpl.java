757a1f7987018e51ee218bb48632a5b2d2fded90
bin.hu <bin.hu@baozun.com>
2016-01-06 15:18:26 +0800

    入库流程-创建PO单

---
+++ warehouse-operation-impl/src/main/java/com/baozun/scm/primservice/whoperation/manager/createspoasn/CreatesPoManagerImpl.java
@@ -0,0 +1,17 @@
+package com.baozun.scm.primservice.whoperation.manager.createspoasn;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+/**
+ * 创建PO单据
+ * 
+ * @author bin.hu
+ * 
+ */
+@Service("createsPoManager")
+@Transactional
+public class CreatesPoManagerImpl implements CreatesPoManager {
+
+}
