ef0b189f4a4fb1ec0f46023e304574dce2618682
bin.hu <bin.hu@baozun.com>
2016-01-13 10:19:51 +0800

    po创建逻辑

--- warehouse-operation-impl/src/main/java/com/baozun/scm/primservice/whoperation/manager/createspoasn/CreatesPoManagerImpl.java
+++ warehouse-operation-impl/src/main/java/com/baozun/scm/primservice/whoperation/manager/createspoasn/CreatesPoManagerImpl.java
@@ -1,6 +1,5 @@
 package com.baozun.scm.primservice.whoperation.manager.createspoasn;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.Date;
 import java.util.Map;
 
@@ -14,10 +13,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.baozun.scm.primservice.whoperation.command.poasn.WhPoCommand;
+import com.baozun.scm.primservice.whoperation.command.poasn.WhPoLineCommand;
 import com.baozun.scm.primservice.whoperation.dao.poasn.WhPoDao;
 import com.baozun.scm.primservice.whoperation.dao.poasn.WhPoLineDao;
 import com.baozun.scm.primservice.whoperation.model.ResponseMsg;
 import com.baozun.scm.primservice.whoperation.model.poasn.WhPo;
+import com.baozun.scm.primservice.whoperation.model.poasn.WhPoLine;
 
 
 /**
@@ -53,6 +54,7 @@ public class CreatesPoManagerImpl implements CreatesPoManager {
         BeanUtils.copyProperties(po, whPo);
         // 相关单据号 调用HUB编码生成器获得
         whPo.setExtCode(String.valueOf(System.currentTimeMillis()));
+        // 采购时间为空默认为当前时间
         if (null == po.getPoDate()) {
             whPo.setPoDate(new Date());
         }
@@ -63,7 +65,24 @@ public class CreatesPoManagerImpl implements CreatesPoManager {
         whPoDao.saveOrUpdate(whPo);
         if (po.getPoLineList().size() > 0) {
             // 有line信息保存
+            for (int i = 0; i < po.getPoLineList().size(); i++) {
+                WhPoLineCommand polineCommand = po.getPoLineList().get(i);
+                WhPoLine poline = new WhPoLine();
+                BeanUtils.copyProperties(polineCommand, poline);
+                poline.setOuId(whPo.getOuId());
+                if (null == poline.getLinenum()) {
+                    // 行号为空的话默认1开始递增
+                    poline.setLinenum(i++);
+                }
+                poline.setCreateTime(new Date());
+                poline.setCreatedId(po.getUserId());
+                poline.setLastModifyTime(new Date());
+                poline.setModifiedId(po.getUserId());
+                whPoLineDao.saveOrUpdate(poline);
+            }
         }
+        rm.setResponseStatus(ResponseMsg.STATUS_SUCCESS);
+        rm.setMsg(whPo.getId() + "");
         return rm;
 
     }
