3089befa8d89ec1cf1e5543d1a3c33221ce63663
bin.hu <bin.hu@baozun.com>
2016-01-15 17:09:09 +0800

    PO单状态

---
+++ warehouse-operation-api/src/main/java/com/baozun/scm/primservice/whoperation/constant/PoAsnStatus.java
@@ -0,0 +1,15 @@
+package com.baozun.scm.primservice.whoperation.constant;
+
+import java.io.Serializable;
+
+/**
+ * Po单Asn单状态常量
+ * 
+ * @author bin.hu
+ * 
+ */
+public class PoAsnStatus implements Serializable {
+
+    private static final long serialVersionUID = -4336754187196074057L;
+
+}
