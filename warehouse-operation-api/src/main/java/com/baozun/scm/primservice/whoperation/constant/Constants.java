ab3f08889a4fe9d8b8bf2f052f631b793038d735
bin.hu <bin.hu@baozun.com>
2016-02-02 17:52:51 +0800

    调编码生成器 constants 常量

--- warehouse-operation-api/src/main/java/com/baozun/scm/primservice/whoperation/constant/Constants.java
+++ warehouse-operation-api/src/main/java/com/baozun/scm/primservice/whoperation/constant/Constants.java
@@ -22,8 +22,19 @@ public final class Constants {
     /** 拆分库 */
     public static final Integer SHARD_SOURCE = 2;
 
+    /** 调编码生成器租户编码 */
     public static final String WMS = "wms";
 
+    /** 调编码生成器 po单实体标识 */
     public static final String WHPO_MODEL_URL = "com.baozun.scm.primservice.whoperation.model.poasn.WhPo";
+
+    /** 调编码生成器 asn单实体标识 */
+    public static final String WHASN_MODEL_URL = "com.baozun.scm.primservice.whoperation.model.poasn.WhAsn";
+
+    /** 调编码生成器 ASN单WMS内部单号分组 */
+    public static final String WMS_ASN_INNER = "WMS_ASN_INNER";
+
+    /** 调编码生成器 ASN单WMS外部单号分组 */
+    public static final String WMS_ASN_EXT = "WMS_ASN_EXT";
 
 }
