c7ca5e5c2bb2e7032ef663d9e7568ea670c72daf
bin.hu <bin.hu@baozun.com>
2016-02-18 11:08:48 +0800

    添加asnlinemanager

---
+++ warehouse-operation-api/src/main/java/com/baozun/scm/primservice/whoperation/manager/poasn/poasnmanager/AsnLineManager.java
@@ -0,0 +1,18 @@
+package com.baozun.scm.primservice.whoperation.manager.poasn.poasnmanager;
+
+import java.util.Map;
+
+import lark.common.dao.Page;
+import lark.common.dao.Pagination;
+import lark.common.dao.Sort;
+
+import com.baozun.scm.primservice.whoperation.command.poasn.WhAsnLineCommand;
+import com.baozun.scm.primservice.whoperation.manager.BaseManager;
+
+public interface AsnLineManager extends BaseManager {
+
+    Pagination<WhAsnLineCommand> findListByQueryMapWithPageExtByInfo(Page page, Sort[] sorts, Map<String, Object> params);
+
+    Pagination<WhAsnLineCommand> findListByQueryMapWithPageExtByShard(Page page, Sort[] sorts, Map<String, Object> params);
+
+}
---
+++ warehouse-operation-impl/src/main/java/com/baozun/scm/primservice/whoperation/manager/poasn/poasnmanager/AsnLineManagerInpl.java
@@ -0,0 +1,28 @@
+package com.baozun.scm.primservice.whoperation.manager.poasn.poasnmanager;
+
+import java.util.Map;
+
+import lark.common.dao.Page;
+import lark.common.dao.Pagination;
+import lark.common.dao.Sort;
+
+import com.baozun.scm.primservice.whoperation.command.poasn.WhAsnLineCommand;
+
+public class AsnLineManagerInpl implements AsnLineManager {
+
+
+    @Override
+    public Pagination<WhAsnLineCommand> findListByQueryMapWithPageExtByInfo(Page page, Sort[] sorts, Map<String, Object> params) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Pagination<WhAsnLineCommand> findListByQueryMapWithPageExtByShard(Page page, Sort[] sorts, Map<String, Object> params) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+
+
+}
