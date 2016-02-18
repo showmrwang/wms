970645e0b0b2550fa83ec020cdd0385705905a68
yimin.lu <yimin.lu@baozun.com>
2016-02-18 13:55:42 +0800

    ASN明细查询

--- warehouse-operation-impl/src/main/java/com/baozun/scm/primservice/whoperation/manager/poasn/poasnmanager/AsnLineManagerImpl.java
+++ warehouse-operation-impl/src/main/java/com/baozun/scm/primservice/whoperation/manager/poasn/poasnmanager/AsnLineManagerImpl.java
@@ -6,26 +6,30 @@ import lark.common.dao.Page;
 import lark.common.dao.Pagination;
 import lark.common.dao.Sort;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.baozun.scm.primservice.whoperation.command.poasn.WhAsnLineCommand;
+import com.baozun.scm.primservice.whoperation.dao.poasn.WhAsnDao;
+import com.baozun.scm.primservice.whoperation.dao.poasn.WhAsnLineDao;
 
 @Service("asnLineManager")
 @Transactional
 public class AsnLineManagerImpl implements AsnLineManager {
-
+    @Autowired
+    private WhAsnDao whAsnDao;
+    @Autowired
+    private WhAsnLineDao whAsnLineDao;
 
     @Override
     public Pagination<WhAsnLineCommand> findListByQueryMapWithPageExtByInfo(Page page, Sort[] sorts, Map<String, Object> params) {
-        // TODO Auto-generated method stub
-        return null;
+        return this.whAsnLineDao.findListByQueryMapWithPageExt(page, sorts, params);
     }
 
     @Override
     public Pagination<WhAsnLineCommand> findListByQueryMapWithPageExtByShard(Page page, Sort[] sorts, Map<String, Object> params) {
-        // TODO Auto-generated method stub
-        return null;
+        return this.whAsnLineDao.findListByQueryMapWithPageExt(page, sorts, params);
     }
 
 
