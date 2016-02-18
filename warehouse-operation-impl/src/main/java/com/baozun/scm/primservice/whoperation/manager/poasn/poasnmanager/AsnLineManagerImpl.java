b04f6c2431f4940fb3dc8b2191b4131ad56330a0
bin.hu <bin.hu@baozun.com>
2016-02-18 13:53:53 +0800

    调整

--- warehouse-operation-impl/src/main/java/com/baozun/scm/primservice/whoperation/manager/poasn/poasnmanager/AsnLineManagerImpl.java
+++ warehouse-operation-impl/src/main/java/com/baozun/scm/primservice/whoperation/manager/poasn/poasnmanager/AsnLineManagerImpl.java
@@ -11,25 +11,23 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.baozun.scm.primservice.whoperation.command.poasn.WhAsnLineCommand;
-import com.baozun.scm.primservice.whoperation.dao.poasn.WhAsnDao;
 import com.baozun.scm.primservice.whoperation.dao.poasn.WhAsnLineDao;
 
 @Service("asnLineManager")
 @Transactional
 public class AsnLineManagerImpl implements AsnLineManager {
-    @Autowired
-    private WhAsnDao whAsnDao;
+    
     @Autowired
     private WhAsnLineDao whAsnLineDao;
 
     @Override
     public Pagination<WhAsnLineCommand> findListByQueryMapWithPageExtByInfo(Page page, Sort[] sorts, Map<String, Object> params) {
-        return this.whAsnLineDao.findListByQueryMapWithPageExt(page, sorts, params);
+        return whAsnLineDao.findListByQueryMapWithPageExt(page, sorts, params);
     }
 
     @Override
     public Pagination<WhAsnLineCommand> findListByQueryMapWithPageExtByShard(Page page, Sort[] sorts, Map<String, Object> params) {
-        return this.whAsnLineDao.findListByQueryMapWithPageExt(page, sorts, params);
+        return whAsnLineDao.findListByQueryMapWithPageExt(page, sorts, params);
     }
 
 
