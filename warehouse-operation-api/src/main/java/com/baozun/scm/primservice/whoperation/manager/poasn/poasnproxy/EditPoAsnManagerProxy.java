077b7bfa35a20b30410ae34e90ab3c179fe9ed39
yimin.lu <yimin.lu@baozun.com>
2016-04-07 15:05:53 +0800

    静态库位配置；库位代码规整；库位BUG修正；PO/ASN接口注释

--- warehouse-operation-api/src/main/java/com/baozun/scm/primservice/whoperation/manager/poasn/poasnproxy/EditPoAsnManagerProxy.java
+++ warehouse-operation-api/src/main/java/com/baozun/scm/primservice/whoperation/manager/poasn/poasnproxy/EditPoAsnManagerProxy.java
@@ -14,29 +14,104 @@ import com.baozun.scm.primservice.whoperation.model.poasn.WhPoLine;
 
 public interface EditPoAsnManagerProxy extends BaseManager {
 
+    /**
+     * 根据ASN的ID,OUID,查找ASN并修改STATUS
+     * 
+     * @param whAsnCommand
+     * @return
+     */
     ResponseMsg editAsnStatus(WhAsnCommand whAsnCommand);
 
+    /**
+     * 取消PO单操作
+     * 
+     * @param whPoCommand
+     * @return
+     */
     ResponseMsg cancelPo(WhPoCommand whPoCommand);
 
+    /**
+     * 修改PO单表头信息
+     * 
+     * @param po
+     * @return
+     */
     ResponseMsg editPo(WhPo po);
 
+    /**
+     * 根据POLINE的POID,OUID,UUID删除PO单明细
+     * 
+     * @param whPoLine
+     */
     void deletePoLineByUuid(WhPoLineCommand whPoLine);
 
+    /**
+     * 修改POLINE
+     * 
+     * @param whPoLine
+     * @return
+     */
     ResponseMsg editPoLine(WhPoLine whPoLine);
 
+    /**
+     * 修改ASNLINE
+     * 
+     * @param whAsnLineCommand
+     * @return
+     */
     ResponseMsg editAsnLine(WhAsnLineCommand whAsnLineCommand);
 
+    /**
+     * 删除POLINE操作
+     * 
+     * @param command
+     */
     void deletePoLines(WhPoLineCommand command);
 
+    /**
+     * 编辑ASN表头信息
+     * 
+     * @param asn
+     * @return
+     */
     ResponseMsg editAsn(WhAsn asn);
 
+    /**
+     * 删除PO单操作
+     * 
+     * @param whPoCommand
+     * @return
+     */
     ResponseMsg deletePoAndPoLine(List<WhPoCommand> whPoCommand);
 
+    /**
+     * 审核PO单操作
+     * 
+     * @param poCommand
+     */
     void auditPo(WhPoCommand poCommand);
 
+    /**
+     * 审核ASN单操作
+     * 
+     * @param asnCommand
+     * @return
+     */
     ResponseMsg auditAsn(WhAsnCommand asnCommand);
 
+    /**
+     * 删除ASN操作（不可批量）
+     * 
+     * @param WhAsnCommand
+     * @return
+     */
     ResponseMsg deleteAsnAndAsnLine(WhAsnCommand WhAsnCommand);
 
+    /**
+     * 删除ASN明细（可批量）
+     * 
+     * @param command
+     * @return
+     */
     ResponseMsg deleteAsnLines(WhAsnLineCommand command);
 }
--- warehouse-operation-api/src/main/java/com/baozun/scm/primservice/whoperation/manager/poasn/poasnproxy/SelectPoAsnManagerProxy.java
+++ warehouse-operation-api/src/main/java/com/baozun/scm/primservice/whoperation/manager/poasn/poasnproxy/SelectPoAsnManagerProxy.java
@@ -15,27 +15,103 @@ import com.baozun.scm.primservice.whoperation.manager.BaseManager;
 
 public interface SelectPoAsnManagerProxy extends BaseManager {
     /**
-     * 一览查询
+     * PO的分页查询
+     * 
+     * @param page
+     * @param sorts
+     * @param params
+     * @param sourceType
+     * @return
      */
     Pagination<WhPoCommand> findWhPoListByQueryMapWithPageExt(Page page, Sort[] sorts, Map<String, Object> params, Integer sourceType);
 
+    /**
+     * 模糊查询方法。 根据asnExtCode,asn状态，仓库查找asn
+     * 
+     * @param asnExtCode
+     * @param status
+     * @param ouid
+     * @return
+     */
     List<WhAsnCommand> findWhAsnListByAsnExtCode(String asnExtCode, Integer status, Long ouid);
 
+    /**
+     * 更加PO单ID，OUID查找PO
+     * 
+     * @param whPoCommand
+     * @return
+     */
     WhPoCommand findWhPoById(WhPoCommand whPoCommand);
 
+    /**
+     * PO单明细的分页查询
+     * 
+     * @param page
+     * @param sorts
+     * @param params
+     * @param sourceType
+     * @return
+     */
     Pagination<WhPoLineCommand> findPoLineListByQueryMapWithPageExt(Page page, Sort[] sorts, Map<String, Object> params, Integer sourceType);
 
+    /**
+     * 根据POLINE的ID,OUID查找PO单明细
+     * 
+     * @param Command
+     * @return
+     */
     WhPoLineCommand findWhPoLineById(WhPoLineCommand Command);
-    
+
+    /**
+     * 根据WhAsnLine的ID,OUID查找PO单明细
+     * 
+     * @param Command
+     * @return
+     */
     WhAsnLineCommand findWhAsnLineById(WhAsnLineCommand Command);
 
+    /**
+     * ASN的分页查询
+     * 
+     * @param page
+     * @param sorts
+     * @param params
+     * @param sourceType
+     * @return
+     */
     Pagination<WhAsnCommand> findWhAsnListByQueryMapWithPageExt(Page page, Sort[] sorts, Map<String, Object> params, Integer sourceType);
 
+    /**
+     * 根据PO的EXTCODE,OUID,STATUSLIST模糊查找PO单明细
+     * 
+     * @param command
+     * @return
+     */
     List<WhPoCommand> findWhPoListByExtCode(WhPoCommand command);
 
+    /**
+     * 生成ASN的EXTCODE
+     * 
+     * @return
+     */
     String getAsnExtCode();
 
+    /**
+     * 根据ASN的ID,OUID查找ASN
+     * 
+     * @param whAsnCommand
+     * @return
+     */
     WhAsnCommand findWhAsnById(WhAsnCommand whAsnCommand);
 
+    /**
+     * ASNLINE的分页查询
+     * 
+     * @param page
+     * @param sorts
+     * @param params
+     * @param sourceType
+     * @return
+     */
     Pagination<WhAsnLineCommand> findAsnLineListByQueryMapWithPageExt(Page page, Sort[] sorts, Map<String, Object> params, Integer sourceType);
 }
