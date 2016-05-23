17ffe9c9915d24c35bc77552cc3c5ed631e0b871
chuan.li <chuan.li@baozun.com>
2016-05-23 19:24:24 +0800

    pad上架

---
+++ warehouse-operation-impl/src/main/java/com/baozun/scm/primservice/whoperation/manager/pda/inbound/putaway/PdaPutawayManagerImpl.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2013 Baozun All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of Baozun.
+ * You shall not disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Baozun.
+ *
+ * BAOZUN MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF THE
+ * SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE, OR NON-INFRINGEMENT. BAOZUN SHALL NOT BE LIABLE FOR ANY DAMAGES
+ * SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING
+ * THIS SOFTWARE OR ITS DERIVATIVES.
+ *
+ */
+package com.baozun.scm.primservice.whoperation.manager.pda.inbound.putaway;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.baozun.scm.primservice.whoperation.manager.BaseManagerImpl;
+
+/**
+ * @author lichuan
+ *
+ */
+@Service("pdaPutawayManager")
+@Transactional
+public class PdaPutawayManagerImpl extends BaseManagerImpl implements PdaPutawayManager {
+    protected static final Logger log = LoggerFactory.getLogger(PdaPutawayManagerImpl.class);
+    
+
+}
