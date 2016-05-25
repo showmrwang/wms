6865ee1865e0583a469d8894442d9469388f709a
yimin.lu <yimin.lu@baozun.com>
2016-05-25 16:36:20 +0800

    PDA通用收货controller and 页面

--- warehouse-operation-impl/src/main/java/com/baozun/scm/primservice/whoperation/manager/pda/rcvd/GeneralRcvdManagerImpl.java
+++ warehouse-operation-impl/src/main/java/com/baozun/scm/primservice/whoperation/manager/pda/rcvd/GeneralRcvdManagerImpl.java
@@ -7,11 +7,19 @@ import org.springframework.transaction.annotation.Transactional;
 import com.baozun.scm.primservice.whoperation.dao.warehouse.inventory.WhSkuInventoryDao;
 import com.baozun.scm.primservice.whoperation.manager.BaseManagerImpl;
 import com.baozun.scm.primservice.whoperation.manager.pda.Rcvd.GeneralRcvdManager;
+import com.baozun.scm.primservice.whoperation.model.warehouse.inventory.WhSkuInventory;
 
 @Service("generalRcvdManager")
 @Transactional
 public class GeneralRcvdManagerImpl extends BaseManagerImpl implements GeneralRcvdManager {
     @Autowired
     private WhSkuInventoryDao whSkuInventoryDao;
+
+    @Override
+    public void saveScanedSkuWhenGeneralRcvdForPda(WhSkuInventory commmand) {
+        // TODO Auto-generated method stub
+
+    }
+
 
 }
