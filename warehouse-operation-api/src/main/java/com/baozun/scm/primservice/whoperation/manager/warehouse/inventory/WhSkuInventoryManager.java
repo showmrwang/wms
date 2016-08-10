93ee7943aeebac8f876dbe988725539fdbca2af4
chuan.li <chuan.li@baozun.com>
2016-08-10 18:54:02 +0800

    库存管理-库位绑定

---
+++ warehouse-operation-api/src/main/java/com/baozun/scm/primservice/whoperation/manager/warehouse/inventory/WhSkuInventoryManager.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2013 Baozun All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of Baozun.
+ * You shall not disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Baozun.
+ *
+ * BAOZUN MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF THE
+ * SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE, OR NON-INFRINGEMENT. BAOZUN SHALL NOT BE LIABLE FOR ANY DAMAGES
+ * SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING
+ * THIS SOFTWARE OR ITS DERIVATIVES.
+ *
+ */
+package com.baozun.scm.primservice.whoperation.manager.warehouse.inventory;
+
+import java.util.List;
+
+import com.baozun.scm.primservice.whoperation.command.pda.inbound.putaway.LocationRecommendResultCommand;
+import com.baozun.scm.primservice.whoperation.command.warehouse.inventory.WhSkuInventoryCommand;
+import com.baozun.scm.primservice.whoperation.manager.BaseManager;
+import com.baozun.scm.primservice.whoperation.model.warehouse.Warehouse;
+
+/**
+ * @author lichuan
+ *
+ */
+public interface WhSkuInventoryManager extends BaseManager {
+    
+    /**
+     * 库位绑定（分配容器库存及生成待移入库位库存）
+     * @author lichuan
+     * @param invList
+     * @param warehouse
+     * @param lrrList
+     * @param putawayPatternDetailType
+     */
+    void containerInvAllocatedAndLocationInvBinding(List<WhSkuInventoryCommand> invList, Warehouse warehouse, List<LocationRecommendResultCommand> lrrList, Integer putawayPatternDetailType, Long ouId, Long userId, String logId);
+
+}
