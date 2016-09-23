4ed61e2f0ef881ce98cca123dd56344c1ca5da17
yimin.lu <yimin.lu@baozun.com>
2016-09-23 11:34:17 +0800

    出库单波次状态

---
+++ warehouse-operation-api/src/main/java/com/baozun/scm/primservice/whoperation/constant/WaveStatus.java
@@ -0,0 +1,19 @@
+package com.baozun.scm.primservice.whoperation.constant;
+
+import java.io.Serializable;
+
+public class WaveStatus implements Serializable {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 2846580922409488154L;
+
+
+    public static final int WAVE_NEW = 1;// 新建；已创建
+    public static final int WAVE_EXECUTING = 5;// 运行中
+    public static final int WAVE_EXECUTED = 10;// 运行完成
+    public static final int WAVE_RELEASE = 15;// 释放
+    public static final int WAVE_CANCEL = 17;// 取消
+    public static final int WAVE_CLOSE = 20;// 关闭
+}
