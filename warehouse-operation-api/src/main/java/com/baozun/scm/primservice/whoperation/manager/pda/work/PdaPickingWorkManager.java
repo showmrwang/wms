2d7c3c1f7017e43f386a9ad6dc5dec9c1eb1de87
ming.tang <ming.tang@baozun.cn>
2016-11-22 17:16:08 +0800

    pda拣货

--- warehouse-operation-api/src/main/java/com/baozun/scm/primservice/whoperation/manager/pda/work/PdaPickingWorkManager.java
+++ warehouse-operation-api/src/main/java/com/baozun/scm/primservice/whoperation/manager/pda/work/PdaPickingWorkManager.java
@@ -1,9 +1,17 @@
 package com.baozun.scm.primservice.whoperation.manager.pda.work;
 
+import com.baozun.scm.primservice.whoperation.command.pda.work.PickingParamCommand;
 import com.baozun.scm.primservice.whoperation.manager.BaseManager;
 
 
 
 public interface PdaPickingWorkManager extends BaseManager {
     
+    /**
+     * pda拣货推荐容器
+     * @param command
+     * @param pickingWay
+     * @return
+     */
+    public PickingParamCommand pdaPickingRemmendContainer(PickingParamCommand command,Integer pickingWay);
 }
--- warehouse-operation-impl/src/main/java/com/baozun/scm/primservice/whoperation/manager/pda/work/PdaPickingWorkManagerImpl.java
+++ warehouse-operation-impl/src/main/java/com/baozun/scm/primservice/whoperation/manager/pda/work/PdaPickingWorkManagerImpl.java
@@ -5,6 +5,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.baozun.scm.primservice.whoperation.command.pda.work.PickingParamCommand;
+import com.baozun.scm.primservice.whoperation.constant.Constants;
 import com.baozun.scm.primservice.whoperation.manager.BaseManagerImpl;
 
 /**
@@ -18,6 +20,25 @@ import com.baozun.scm.primservice.whoperation.manager.BaseManagerImpl;
 public class PdaPickingWorkManagerImpl extends BaseManagerImpl implements PdaPickingWorkManager {
 
     protected static final Logger log = LoggerFactory.getLogger(PdaPickingWorkManagerImpl.class);
+
+    @Override
+    public PickingParamCommand pdaPickingRemmendContainer(PickingParamCommand command, Integer pickingWay) {
+        log.info("PdaPickingWorkManagerImpl pdaPickingRemmendContainer is start");
+        if(pickingWay == Constants.PICKING_WAY_ONE) { //使用外部容器(小车) 无出库箱拣货流程
+            
+        }
+        if(pickingWay == Constants.PICKING_WAY_TWO) { //使用外部(小车)，有出库箱拣货流程
+            
+        }
+        if(pickingWay == Constants.PICKING_WAY_THREE) { //使用出库箱拣货流程
+            
+        }
+        if(pickingWay == Constants.PICKING_WAY_FOUR) {  //使用周转箱拣货流程
+            
+        }
+        log.info("PdaPickingWorkManagerImpl pdaPickingRemmendContainer is end");
+        return null;
+    }
 
     
 }
