09dbe2ca64285ac9e5d8d4f9c58dd33b0f7eb84c
qiming.liu <kJd6*NGk>
2016-11-22 17:03:32 +0800

    作业明细统计

---
+++ warehouse-operation-api/src/main/java/com/baozun/scm/primservice/whoperation/command/pda/work/OperatioLineStatisticsCommand.java
@@ -0,0 +1,49 @@
+package com.baozun.scm.primservice.whoperation.command.pda.work;
+
+import com.baozun.scm.primservice.whoperation.command.BaseCommand;
+/**
+ * 作业明细统计
+ * 
+ * @author qiming.liu
+ *
+ */
+public class OperatioLineStatisticsCommand extends BaseCommand{
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -9131853146971418614L;
+
+    
+    /** 流程相关统计信息 **/
+    
+    
+    /** 是否整托整箱 */
+    /** 所有小车 */
+    /** 所有出库箱 */
+    /** 小车货格与出库箱对应关系 */
+    /** 所有周转箱 */
+    /** 所有托盘 */
+    /** 所有货箱 */
+    
+    
+    /** 库位商品统计信息 **/
+    
+    
+    /** 所有库位 */
+    /** 库位上所有外部容器 */
+    /** 库位上所有内部容器 */
+    /** 库位上所有sku */
+    /** 库位上每个sku总件数 */
+    /** 库位上每个sku对应的唯一sku及件数 */
+    /** 库位上每个唯一sku对应的所有sn及残次条码 */
+    /** 库位上每个唯一sku对应的货格（is_whole_case=0&&有小车&&库位上sku不在任何容器内） */
+    /** 外部容器对应所有内部容器 */
+    /** 内部容器对应所有sku*/
+    /** 内部容器每个sku总件数 */
+    /** 内部容器每个sku对应的唯一sku及件数 */
+    /** 内部容器每个唯一sku对应的所有sn及残次条码 */
+    /** 内部容器每个唯一sku对应的货格（is_whole_case=0&&有小车） */
+    
+    
+}
