01a422bee965398aea62ae710090a8b1e1e4056e
qiming.liu <kJd6*NGk>
2016-11-23 15:57:33 +0800

    作业明细统计结果--第二版

--- warehouse-operation-api/src/main/java/com/baozun/scm/primservice/whoperation/command/pda/work/OperatioLineStatisticsCommand.java
+++ warehouse-operation-api/src/main/java/com/baozun/scm/primservice/whoperation/command/pda/work/OperatioLineStatisticsCommand.java
@@ -27,17 +27,17 @@ public class OperatioLineStatisticsCommand extends BaseCommand{
     /** 是否整托整箱 */
     private Boolean isWholeCase;
     /** 所有小车 */
-    private Set<Long> outerContainerList = new HashSet<Long>();
+    private Set<Long> outerContainers = new HashSet<Long>();
     /** 所有出库箱 */
-    private Set<Long> outbounxBoxList = new HashSet<Long>();
+    private Set<Long> outbounxBoxs = new HashSet<Long>();
     /** 小车货格与出库箱对应关系 */
-    private Set<Long> childList = new HashSet<Long>();
+    private Set<Long> childLists = new HashSet<Long>();//需要确认
     /** 所有周转箱 */
-    private Set<Long> turnoverBoxList = new HashSet<Long>();
+    private Set<Long> turnoverBoxs = new HashSet<Long>();
     /** 所有托盘 */
-    private Set<Long> palletList = new HashSet<Long>();
+    private Set<Long> pallets = new HashSet<Long>();
     /** 所有货箱 */
-    private Set<Long> containerList = new HashSet<Long>();
+    private Set<Long> containers= new HashSet<Long>();
     
     // 库位商品统计信息 
     /** 所有库位 */
@@ -49,69 +49,75 @@ public class OperatioLineStatisticsCommand extends BaseCommand{
     /** 库位上所有sku */
     private Map<Long, Set<Long>> skuIds = new HashMap<Long, Set<Long>>();
     /** 库位上每个sku总件数 */
-    private Map<Long, Map<Long, Set<Long>>> skuQty = new HashMap<Long, Map<Long, Set<Long>>>();;
+    private Map<Long, Long> skuQty = new HashMap<Long, Long>();
     /** 库位上每个sku对应的唯一sku及件数 */
-    private Map<Long, Set<Long>> skuAttrIds = new HashMap<Long, Set<Long>>();
+    private Map<Long, Map<String, Long>> skuAttrIds = new HashMap<Long, Map<String, Long>>();
     /** 库位上每个唯一sku对应的所有sn及残次条码 */
-    private Map<Long, Map<Long, Set<Long>>> skuAttrIdsSnDefect = new HashMap<Long, Map<Long, Set<Long>>>();
+    private Map<Long, Map<String, Set<String>>> skuAttrIdsSnDefect = new HashMap<Long, Map<String, Set<String>>>();
     /** 库位上每个唯一sku对应的货格（is_whole_case=0&&有小车&&库位上sku不在任何容器内） */
-    private Map<Long, Map<Long, Set<Long>>> skuAttrIdsContainerLattice = new HashMap<Long, Map<Long, Set<Long>>>();
+    private Map<Long, Map<String, Set<String>>> skuAttrIdsContainerLattice = new HashMap<Long, Map<String, Set<String>>>();//需要确认
     /** 外部容器对应所有内部容器 */
-    private Map<Long, Map<Long, Set<Long>>> outerToInside;
+    private Map<Long, Set<Long>> outerToInside = new HashMap<Long, Set<Long>>();
     /** 内部容器对应所有sku*/
-    private Map<Long, Map<Long, Map<Long, Set<Long>>>> insideSkuIds;
+    private Map<Long, Set<Long>> insideSkuIds = new HashMap<Long, Set<Long>>();
     /** 内部容器每个sku总件数 */
-    private Map<Long, Map<Long, Map<Long, Set<Long>>>> insideSkuQty;
+    private Map<Long, Long> insideSkuQty = new HashMap<Long, Long>();
     /** 内部容器每个sku对应的唯一sku及件数 */
-    private Map<Long, Map<Long, Map<Long, Set<Long>>>> insideSkuAttrIds;
+    private Map<Long, Map<String, Long>> insideSkuAttrIds = new HashMap<Long, Map<String, Long>>();
     /** 内部容器每个唯一sku对应的所有sn及残次条码 */
-    private Map<Long, Map<Long, Map<Long, Map<Long, Set<Long>>>>> insideSkuAttrIdsSnDefect;
+    private Map<Long, Map<String, Set<String>>> insideSkuAttrIdsSnDefect = new HashMap<Long, Map<String, Set<String>>>();
     /** 内部容器每个唯一sku对应的货格（is_whole_case=0&&有小车） */
-    private Map<Long, Map<Long, Map<Long, Map<Long, Set<Long>>>>> insideSkuAttrIdsContainerLattice;
+    private Map<Long, Map<String, Set<String>>> insideSkuAttrIdsContainerLattice = new HashMap<Long, Map<String, Set<String>>>();
     
     //columns END
 
+    public Long getPickingWay() {
+        return pickingWay;
+    }
+    public void setPickingWay(Long pickingWay) {
+        this.pickingWay = pickingWay;
+    }
     public Boolean getIsWholeCase() {
         return isWholeCase;
     }
     public void setIsWholeCase(Boolean isWholeCase) {
         this.isWholeCase = isWholeCase;
     }
-    public Set<Long> getOuterContainerList() {
-        return outerContainerList;
+    public Set<Long> getOuterContainers() {
+        return outerContainers;
     }
-    public void setOuterContainerList(Set<Long> outerContainerList) {
-        this.outerContainerList = outerContainerList;
+    public void setOuterContainers(Set<Long> outerContainers) {
+        this.outerContainers = outerContainers;
     }
-    public Set<Long> getOutbounxBoxList() {
-        return outbounxBoxList;
+    public Set<Long> getOutbounxBoxs() {
+        return outbounxBoxs;
     }
-    public void setOutbounxBoxList(Set<Long> outbounxBoxList) {
-        this.outbounxBoxList = outbounxBoxList;
+    public void setOutbounxBoxs(Set<Long> outbounxBoxs) {
+        this.outbounxBoxs = outbounxBoxs;
     }
-    public Set<Long> getChildList() {
-        return childList;
+    public Set<Long> getChildLists() {
+        return childLists;
     }
-    public void setChildList(Set<Long> childList) {
-        this.childList = childList;
+    public void setChildLists(Set<Long> childLists) {
+        this.childLists = childLists;
     }
-    public Set<Long> getTurnoverBoxList() {
-        return turnoverBoxList;
+    public Set<Long> getTurnoverBoxs() {
+        return turnoverBoxs;
     }
-    public void setTurnoverBoxList(Set<Long> turnoverBoxList) {
-        this.turnoverBoxList = turnoverBoxList;
+    public void setTurnoverBoxs(Set<Long> turnoverBoxs) {
+        this.turnoverBoxs = turnoverBoxs;
     }
-    public Set<Long> getPalletList() {
-        return palletList;
+    public Set<Long> getPallets() {
+        return pallets;
     }
-    public void setPalletList(Set<Long> palletList) {
-        this.palletList = palletList;
+    public void setPallets(Set<Long> pallets) {
+        this.pallets = pallets;
     }
-    public Set<Long> getContainerList() {
-        return containerList;
+    public Set<Long> getContainers() {
+        return containers;
     }
-    public void setContainerList(Set<Long> containerList) {
-        this.containerList = containerList;
+    public void setContainers(Set<Long> containers) {
+        this.containers = containers;
     }
     public Set<Long> getLocationIdList() {
         return locationIdList;
@@ -137,71 +143,64 @@ public class OperatioLineStatisticsCommand extends BaseCommand{
     public void setSkuIds(Map<Long, Set<Long>> skuIds) {
         this.skuIds = skuIds;
     }
-    public Map<Long, Map<Long, Set<Long>>> getSkuQty() {
+    public Map<Long, Long> getSkuQty() {
         return skuQty;
     }
-    public void setSkuQty(Map<Long, Map<Long, Set<Long>>> skuQty) {
+    public void setSkuQty(Map<Long, Long> skuQty) {
         this.skuQty = skuQty;
     }
-    public Map<Long, Set<Long>> getSkuAttrIds() {
+    public Map<Long, Map<String, Long>> getSkuAttrIds() {
         return skuAttrIds;
     }
-    public void setSkuAttrIds(Map<Long, Set<Long>> skuAttrIds) {
+    public void setSkuAttrIds(Map<Long, Map<String, Long>> skuAttrIds) {
         this.skuAttrIds = skuAttrIds;
     }
-    public Map<Long, Map<Long, Set<Long>>> getSkuAttrIdsSnDefect() {
+    public Map<Long, Map<String, Set<String>>> getSkuAttrIdsSnDefect() {
         return skuAttrIdsSnDefect;
     }
-    public void setSkuAttrIdsSnDefect(Map<Long, Map<Long, Set<Long>>> skuAttrIdsSnDefect) {
+    public void setSkuAttrIdsSnDefect(Map<Long, Map<String, Set<String>>> skuAttrIdsSnDefect) {
         this.skuAttrIdsSnDefect = skuAttrIdsSnDefect;
     }
-    public Map<Long, Map<Long, Set<Long>>> getSkuAttrIdsContainerLattice() {
+    public Map<Long, Map<String, Set<String>>> getSkuAttrIdsContainerLattice() {
         return skuAttrIdsContainerLattice;
     }
-    public void setSkuAttrIdsContainerLattice(Map<Long, Map<Long, Set<Long>>> skuAttrIdsContainerLattice) {
+    public void setSkuAttrIdsContainerLattice(Map<Long, Map<String, Set<String>>> skuAttrIdsContainerLattice) {
         this.skuAttrIdsContainerLattice = skuAttrIdsContainerLattice;
     }
-    public Map<Long, Map<Long, Set<Long>>> getOuterToInside() {
+    public Map<Long, Set<Long>> getOuterToInside() {
         return outerToInside;
     }
-    public void setOuterToInside(Map<Long, Map<Long, Set<Long>>> outerToInside) {
+    public void setOuterToInside(Map<Long, Set<Long>> outerToInside) {
         this.outerToInside = outerToInside;
     }
-    public Map<Long, Map<Long, Map<Long, Set<Long>>>> getInsideSkuIds() {
+    public Map<Long, Set<Long>> getInsideSkuIds() {
         return insideSkuIds;
     }
-    public void setInsideSkuIds(Map<Long, Map<Long, Map<Long, Set<Long>>>> insideSkuIds) {
+    public void setInsideSkuIds(Map<Long, Set<Long>> insideSkuIds) {
         this.insideSkuIds = insideSkuIds;
     }
-    public Map<Long, Map<Long, Map<Long, Set<Long>>>> getInsideSkuQty() {
+    public Map<Long, Long> getInsideSkuQty() {
         return insideSkuQty;
     }
-    public void setInsideSkuQty(Map<Long, Map<Long, Map<Long, Set<Long>>>> insideSkuQty) {
+    public void setInsideSkuQty(Map<Long, Long> insideSkuQty) {
         this.insideSkuQty = insideSkuQty;
     }
-    public Map<Long, Map<Long, Map<Long, Set<Long>>>> getInsideSkuAttrIds() {
+    public Map<Long, Map<String, Long>> getInsideSkuAttrIds() {
         return insideSkuAttrIds;
     }
-    public void setInsideSkuAttrIds(Map<Long, Map<Long, Map<Long, Set<Long>>>> insideSkuAttrIds) {
+    public void setInsideSkuAttrIds(Map<Long, Map<String, Long>> insideSkuAttrIds) {
         this.insideSkuAttrIds = insideSkuAttrIds;
     }
-    public Map<Long, Map<Long, Map<Long, Map<Long, Set<Long>>>>> getInsideSkuAttrIdsSnDefect() {
+    public Map<Long, Map<String, Set<String>>> getInsideSkuAttrIdsSnDefect() {
         return insideSkuAttrIdsSnDefect;
     }
-    public void setInsideSkuAttrIdsSnDefect(Map<Long, Map<Long, Map<Long, Map<Long, Set<Long>>>>> insideSkuAttrIdsSnDefect) {
+    public void setInsideSkuAttrIdsSnDefect(Map<Long, Map<String, Set<String>>> insideSkuAttrIdsSnDefect) {
         this.insideSkuAttrIdsSnDefect = insideSkuAttrIdsSnDefect;
     }
-    public Map<Long, Map<Long, Map<Long, Map<Long, Set<Long>>>>> getInsideSkuAttrIdsContainerLattice() {
+    public Map<Long, Map<String, Set<String>>> getInsideSkuAttrIdsContainerLattice() {
         return insideSkuAttrIdsContainerLattice;
     }
-    public void setInsideSkuAttrIdsContainerLattice(Map<Long, Map<Long, Map<Long, Map<Long, Set<Long>>>>> insideSkuAttrIdsContainerLattice) {
+    public void setInsideSkuAttrIdsContainerLattice(Map<Long, Map<String, Set<String>>> insideSkuAttrIdsContainerLattice) {
         this.insideSkuAttrIdsContainerLattice = insideSkuAttrIdsContainerLattice;
     }
-    public Long getPickingWay() {
-        return pickingWay;
-    }
-    public void setPickingWay(Long pickingWay) {
-        this.pickingWay = pickingWay;
-    }
-    
 }
