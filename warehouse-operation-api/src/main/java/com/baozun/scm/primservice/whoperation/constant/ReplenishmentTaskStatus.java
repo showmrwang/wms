0f695d72fe7c40a500ee633f021e33d56faa10ac
yimin.lu <yimin.lu@baozun.com>
2016-12-20 17:54:12 +0800

    波次：详情；取消；释放；

---
+++ warehouse-operation-api/src/main/java/com/baozun/scm/primservice/whoperation/constant/ReplenishmentTaskStatus.java
@@ -0,0 +1,15 @@
+package com.baozun.scm.primservice.whoperation.constant;
+
+import java.io.Serializable;
+
+public class ReplenishmentTaskStatus implements Serializable {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1686866249274945855L;
+
+    public static final int REPLENISHMENT_TASK_NEW = 1;// 新建
+    public static final int REPLENISHMENT_TASK_CANCEL = 17;// 取消
+
+}
