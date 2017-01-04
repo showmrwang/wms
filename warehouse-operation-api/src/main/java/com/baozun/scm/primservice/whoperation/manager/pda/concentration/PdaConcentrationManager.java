c0407118b63d1d573a5e5de4b44b9bf2ae6d97b5
kai.zhu <kai.zhu@baozun.cn>
2017-01-04 16:27:19 +0800

    集货

---
+++ warehouse-operation-api/src/main/java/com/baozun/scm/primservice/whoperation/manager/pda/concentration/PdaConcentrationManager.java
@@ -0,0 +1,12 @@
+package com.baozun.scm.primservice.whoperation.manager.pda.concentration;
+
+import com.baozun.scm.primservice.whoperation.manager.BaseManager;
+
+/**
+ * PDA-集货
+ * @author jumbo
+ *
+ */
+public interface PdaConcentrationManager extends BaseManager {
+
+}
---
+++ warehouse-operation-impl/src/main/java/com/baozun/scm/primservice/whoperation/manager/pda/concentration/PdaConcentrationManagerImpl.java
@@ -0,0 +1,7 @@
+package com.baozun.scm.primservice.whoperation.manager.pda.concentration;
+
+import com.baozun.scm.primservice.whoperation.manager.BaseManagerImpl;
+
+public class PdaConcentrationManagerImpl extends BaseManagerImpl implements PdaConcentrationManager {
+
+}
