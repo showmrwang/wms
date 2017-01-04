2047e9c56524c55d64d0df09dce4c394eeaac2e5
kai.zhu <kai.zhu@baozun.cn>
2017-01-04 16:42:40 +0800

    集货

--- warehouse-operation-impl/src/main/java/com/baozun/scm/primservice/whoperation/manager/pda/concentration/PdaConcentrationManagerImpl.java
+++ warehouse-operation-impl/src/main/java/com/baozun/scm/primservice/whoperation/manager/pda/concentration/PdaConcentrationManagerImpl.java
@@ -1,7 +1,10 @@
 package com.baozun.scm.primservice.whoperation.manager.pda.concentration;
 
+import org.springframework.stereotype.Service;
+
 import com.baozun.scm.primservice.whoperation.manager.BaseManagerImpl;
 
+@Service("pdaConcentrationManager")
 public class PdaConcentrationManagerImpl extends BaseManagerImpl implements PdaConcentrationManager {
 
 }
