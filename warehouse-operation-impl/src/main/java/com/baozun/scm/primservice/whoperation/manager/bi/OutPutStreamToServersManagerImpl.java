ccf0e50ea3cb81be7bb3e803a02885305c068ad3
bin.hu <bin.hu@baozun.com>
2017-01-05 09:38:52 +0800

    导入EXCEL上传

---
+++ warehouse-operation-impl/src/main/java/com/baozun/scm/primservice/whoperation/manager/bi/OutPutStreamToServersManagerImpl.java
@@ -0,0 +1,94 @@
+package com.baozun.scm.primservice.whoperation.manager.bi;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.I0Itec.zkclient.ZkClient;
+import org.apache.zookeeper.data.Stat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.baozun.scm.primservice.whoperation.constant.Constants;
+import com.baozun.scm.primservice.whoperation.manager.BaseManagerImpl;
+import com.baozun.scm.primservice.whoperation.manager.odo.bi.UserImportExcelDao;
+import com.baozun.scm.primservice.whoperation.model.bi.ImportExcel;
+import com.baozun.scm.primservice.whoperation.model.bi.UserImportExcel;
+import com.baozun.scm.primservice.whoperation.util.DateUtil;
+
+@Service("outPutStreamToServersManager")
+@Transactional
+public class OutPutStreamToServersManagerImpl extends BaseManagerImpl implements OutPutStreamToServersManager {
+
+    protected static final Logger log = LoggerFactory.getLogger(OutPutStreamToServersManager.class);
+
+    @Autowired
+    private ZkClient zkClient;
+    @Autowired
+    private UserImportExcelDao userImportExcelDao;
+
+    // 获取zk信息
+    @Value("${zk.cofing.root}")
+    private String znode;
+
+
+    /**
+     * 上传导入验证失败文件到服务器 InputStream in 文件流 importType 文件类型 导入类型
+     */
+    @Override
+    public String uploadImportFileError(ImportExcel ie) {
+        String returnString = "";
+        String fileName = "";
+        FileOutputStream os = null;
+        try {
+            // 获取ZK 上传URL
+            Stat stat = new Stat();
+            Object object = zkClient.readData(znode + Constants.IMPORT_EXCEL_ZK_ERROR_URL, stat);
+            String dateString = DateUtil.getSysDateFormat("yyyyMMddHHmmss");
+            String url = object.toString();
+            // 文件名称格式:导入类型+当前时间+USERID
+            fileName = ie.getImportType() + "_" + dateString + "_" + ie.getUserId() + "_" + Constants.ERROR + Constants.EXPORT_EXCEL_XLSX;
+            returnString = fileName;
+            File filedir = new File(url);
+            if (!filedir.exists()) {
+                filedir.mkdirs();
+            }
+            File file = new File(filedir, fileName);
+            os = new FileOutputStream(file);
+            ie.getWorkbook().write(os);
+            // 更新导入用户信息表数据
+            UserImportExcel u = userImportExcelDao.findUserImportExcelByIdAndOuId(ie.getUserImportExcelId(), ie.getOuId());
+            if (null == u) {
+                log.error("uploadImportFileError Error UserImportExcel is null UserImportExcelId: " + ie.getUserImportExcelId());
+                returnString = Constants.ERROR;
+                return returnString;
+            }
+            u.setErrorFileName(fileName);
+            userImportExcelDao.saveOrUpdate(u);
+            // byte[] byteStr = new byte[2048];
+            // int len = 0;
+            // while ((len = in.read(byteStr)) > 0) {
+            // os.write(byteStr, 0, len);
+            // }
+        } catch (Exception e) {
+            log.error("uploadImportFileError Error：", e);
+            returnString = Constants.ERROR;
+        } finally {
+            try {
+                if (null != os) {
+                    os.flush();
+                    os.close();
+                }
+            } catch (IOException e) {
+                log.error("uploadImportFileError Error：", e);
+                returnString = Constants.ERROR;
+            }
+        }
+        return returnString;
+    }
+
+}
---
+++ warehouse-operation-impl/src/main/java/com/baozun/scm/primservice/whoperation/manager/odo/bi/UserImportExcelDao.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2013 Baozun All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of Baozun. You shall not disclose
+ * such Confidential Information and shall use it only in accordance with the terms of the license
+ * agreement you entered into with Baozun.
+ *
+ * BAOZUN MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF THE SOFTWARE, EITHER
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE, OR NON-INFRINGEMENT. BAOZUN SHALL NOT BE LIABLE FOR ANY DAMAGES
+ * SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS
+ * DERIVATIVES.
+ *
+ */
+package com.baozun.scm.primservice.whoperation.manager.odo.bi;
+
+import java.util.List;
+import java.util.Map;
+
+import lark.common.annotation.CommonQuery;
+import lark.common.annotation.QueryPage;
+import lark.common.dao.Page;
+import lark.common.dao.Pagination;
+import lark.common.dao.QueryCondition;
+import lark.common.dao.Sort;
+import lark.orm.dao.supports.BaseDao;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.baozun.scm.primservice.whoperation.model.bi.UserImportExcel;
+
+public interface UserImportExcelDao extends BaseDao<UserImportExcel, Long> {
+
+
+    @QueryPage("findListCountByQueryMap")
+    Pagination<UserImportExcel> findListByQueryMapWithPage(Page page, Sort[] sorts, Map<String, Object> params);
+
+    @QueryPage("queryCount")
+    Pagination<UserImportExcel> query(Page page, Sort[] sorts, QueryCondition cond);
+
+    List<UserImportExcel> query(QueryCondition cond);
+
+    Long queryCount(QueryCondition cond);
+
+    @CommonQuery
+    int saveOrUpdate(UserImportExcel o);
+
+    /**
+     * 根据ID+OUID查询对应数据
+     * 
+     * @return
+     */
+    UserImportExcel findUserImportExcelByIdAndOuId(@Param("id") Long id, @Param("ouid") Long ouid);
+
+}
