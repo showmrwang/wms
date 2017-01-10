05aacbee9b7feb8e6a0bc06aa45d897a67832ac2
gianni.zhang <gianni.zhang@baozun.com>
2017-01-10 15:59:41 +0800

    集货对象

--- warehouse-operation-api/src/main/java/com/baozun/scm/primservice/whoperation/command/pda/collection/WorkCollectionCommand.java
+++ warehouse-operation-api/src/main/java/com/baozun/scm/primservice/whoperation/command/pda/collection/WorkCollectionCommand.java
@@ -1,6 +1,9 @@
 package com.baozun.scm.primservice.whoperation.command.pda.collection;
 
+import java.util.List;
+
 import com.baozun.scm.primservice.whoperation.command.BaseCommand;
+import com.baozun.scm.primservice.whoperation.command.odo.wave.RecFacilityPathCommand;
 
 public class WorkCollectionCommand extends BaseCommand {
 
@@ -13,6 +16,18 @@ public class WorkCollectionCommand extends BaseCommand {
     private Long ouId;
     /** 批次号*/
     private String batch;
+    /** 容器编码*/
+    private String containerCode;
+    /** 容器id*/
+    private Long containerId;
+    /**  播种墙推荐逻辑*/
+    private List<RecFacilityPathCommand> rfp;
+    /** 成功/失败*/
+    private Boolean isSuccess;
+    /** 目标位置:播种墙, 出库暂存库位, 中转库位*/
+    private String targetPos;
+    /** 是否扫描校验码*/
+    private Boolean isScanCheckCode;
 
     public Long getOuId() {
         return ouId;
@@ -30,6 +45,52 @@ public class WorkCollectionCommand extends BaseCommand {
         this.batch = batch;
     }
 
+    public List<RecFacilityPathCommand> getRfp() {
+        return rfp;
+    }
 
+    public void setRfp(List<RecFacilityPathCommand> rfp) {
+        this.rfp = rfp;
+    }
+
+    public Boolean getIsSuccess() {
+        return isSuccess;
+    }
+
+    public void setIsSuccess(Boolean isSuccess) {
+        this.isSuccess = isSuccess;
+    }
+
+    public String getContainerCode() {
+        return containerCode;
+    }
+
+    public void setContainerCode(String containerCode) {
+        this.containerCode = containerCode;
+    }
+
+    public Long getContainerId() {
+        return containerId;
+    }
+
+    public void setContainerId(Long containerId) {
+        this.containerId = containerId;
+    }
+
+    public String getTargetPos() {
+        return targetPos;
+    }
+
+    public void setTargetPos(String targetPos) {
+        this.targetPos = targetPos;
+    }
+
+    public Boolean getIsScanCheckCode() {
+        return isScanCheckCode;
+    }
+
+    public void setIsScanCheckCode(Boolean isScanCheckCode) {
+        this.isScanCheckCode = isScanCheckCode;
+    }
 
 }
--- warehouse-operation-api/src/main/java/com/baozun/scm/primservice/whoperation/command/warehouse/WhSeedingCollectionCommand.java
+++ warehouse-operation-api/src/main/java/com/baozun/scm/primservice/whoperation/command/warehouse/WhSeedingCollectionCommand.java
@@ -1,17 +1,15 @@
 /**
  * Copyright (c) 2013 Baozun All Rights Reserved.
  *
- * This software is the confidential and proprietary information of Baozun.
- * You shall not disclose such Confidential Information and shall use it only in
- * accordance with the terms of the license agreement you entered into
- * with Baozun.
+ * This software is the confidential and proprietary information of Baozun. You shall not disclose
+ * such Confidential Information and shall use it only in accordance with the terms of the license
+ * agreement you entered into with Baozun.
  *
- * BAOZUN MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF THE
- * SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
- * PURPOSE, OR NON-INFRINGEMENT. BAOZUN SHALL NOT BE LIABLE FOR ANY DAMAGES
- * SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING
- * THIS SOFTWARE OR ITS DERIVATIVES.
+ * BAOZUN MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF THE SOFTWARE, EITHER
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE, OR NON-INFRINGEMENT. BAOZUN SHALL NOT BE LIABLE FOR ANY DAMAGES
+ * SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS
+ * DERIVATIVES.
  *
  */
 package com.baozun.scm.primservice.whoperation.command.warehouse;
@@ -24,85 +22,109 @@ import com.baozun.scm.primservice.whoperation.command.BaseCommand;
  *
  */
 public class WhSeedingCollectionCommand extends BaseCommand {
-	
-	private static final long serialVersionUID = 8610455402243874039L;
-	
-	// columns START
-	/** id */
-	private Long id;
-	/** 播种墙ID */
-	private Long facilityId;
-	/** 暂存库位ID */
-	private Long temporaryLocationId;
-	/** 中转库位ID */
-	private Long locationId;
-	/** 周转箱ID */
-	private Long containerId;
-	/** 小批次 */
-	private String batch;
-	/** 对应组织ID */
-	private Long ouId;
-	// columns END
 
-	public WhSeedingCollectionCommand() {}
+    private static final long serialVersionUID = 8610455402243874039L;
 
-	public WhSeedingCollectionCommand(Long id) {
-		this.id = id;
-	}
+    // columns START
+    /** id */
+    private Long id;
+    /** 播种墙ID */
+    private Long facilityId;
+    /** 暂存库位ID */
+    private Long temporaryLocationId;
+    /** 中转库位ID */
+    private Long locationId;
+    /** 周转箱ID */
+    private Long containerId;
+    /** 小批次 */
+    private String batch;
+    /** 播种状态*/
+    private Integer seedingStatus;
+    /** 对应组织ID */
+    private Long ouId;
 
-	public Long getId() {
-		return id;
-	}
+    // ===============自定义字段===============
+    /** 播种墙编码*/
+    private String seedingwallCode;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    // columns END
 
-	public void setFacilityId(Long value) {
-		this.facilityId = value;
-	}
+    public WhSeedingCollectionCommand() {}
 
-	public Long getFacilityId() {
-		return this.facilityId;
-	}
+    public WhSeedingCollectionCommand(Long id) {
+        this.id = id;
+    }
 
-	public void setTemporaryLocationId(Long value) {
-		this.temporaryLocationId = value;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public Long getTemporaryLocationId() {
-		return this.temporaryLocationId;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public void setLocationId(Long value) {
-		this.locationId = value;
-	}
+    public void setFacilityId(Long value) {
+        this.facilityId = value;
+    }
 
-	public Long getLocationId() {
-		return this.locationId;
-	}
+    public Long getFacilityId() {
+        return this.facilityId;
+    }
 
-	public void setContainerId(Long value) {
-		this.containerId = value;
-	}
+    public void setTemporaryLocationId(Long value) {
+        this.temporaryLocationId = value;
+    }
 
-	public Long getContainerId() {
-		return this.containerId;
-	}
+    public Long getTemporaryLocationId() {
+        return this.temporaryLocationId;
+    }
 
-	public void setBatch(String value) {
-		this.batch = value;
-	}
+    public void setLocationId(Long value) {
+        this.locationId = value;
+    }
 
-	public String getBatch() {
-		return this.batch;
-	}
+    public Long getLocationId() {
+        return this.locationId;
+    }
 
-	public void setOuId(Long value) {
-		this.ouId = value;
-	}
+    public void setContainerId(Long value) {
+        this.containerId = value;
+    }
 
-	public Long getOuId() {
-		return this.ouId;
-	}
+    public Long getContainerId() {
+        return this.containerId;
+    }
+
+    public void setBatch(String value) {
+        this.batch = value;
+    }
+
+    public String getBatch() {
+        return this.batch;
+    }
+
+    public void setOuId(Long value) {
+        this.ouId = value;
+    }
+
+    public Long getOuId() {
+        return this.ouId;
+    }
+
+    public String getSeedingwallCode() {
+        return seedingwallCode;
+    }
+
+    public void setSeedingwallCode(String seedingwallCode) {
+        this.seedingwallCode = seedingwallCode;
+    }
+
+    public Integer getSeedingStatus() {
+        return seedingStatus;
+    }
+
+    public void setSeedingStatus(Integer seedingStatus) {
+        this.seedingStatus = seedingStatus;
+    }
+
 }
