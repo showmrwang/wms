9b13b84c32fc2bf92c68527d9a1d25d459d461f2
gianni.zhang <gianni.zhang@baozun.com>
2017-01-12 14:54:52 +0800

    uuid 添加 outboundboxcode

--- warehouse-operation-impl/src/main/java/com/baozun/scm/primservice/whoperation/util/SkuInventoryUuid.java
+++ warehouse-operation-impl/src/main/java/com/baozun/scm/primservice/whoperation/util/SkuInventoryUuid.java
@@ -21,17 +21,18 @@ public class SkuInventoryUuid {
             return uuid;
         }
         // 拼接库存对应字段值
-        String forMatString = inv.getSkuId().toString() + "" + (inv.getLocationId() == null ? "" : inv.getLocationId().toString()) + "" + (inv.getOuterContainerId() == null ? "" : inv.getOuterContainerId().toString()) + ""
-                + (inv.getInsideContainerId() == null ? "" : inv.getInsideContainerId().toString()) + inv.getCustomerId().toString() + inv.getStoreId().toString() + inv.getInvStatus().toString() + ""
-                + (inv.getInvType() == null ? "" : inv.getInvType().trim().toString()) + "" + (inv.getBatchNumber() == null ? "" : inv.getBatchNumber().trim().toString()) + ""
-                + (inv.getMfgDate() == null ? "" : new SimpleDateFormat("yyyyMMddHHmmss").format(inv.getMfgDate())) + "" + (inv.getExpDate() == null ? "" : new SimpleDateFormat("yyyyMMddHHmmss").format(inv.getExpDate())) + ""
-                + (inv.getCountryOfOrigin() == null ? "" : inv.getCountryOfOrigin().trim().toString()) + "" + (inv.getInvAttr1() == null ? "" : inv.getInvAttr1().trim().toString()) + ""
-                + (inv.getInvAttr2() == null ? "" : inv.getInvAttr2().trim().toString()) + "" + (inv.getInvAttr3() == null ? "" : inv.getInvAttr3().trim().toString()) + "" + (inv.getInvAttr4() == null ? "" : inv.getInvAttr4().trim().toString()) + ""
-                + (inv.getInvAttr5() == null ? "" : inv.getInvAttr5().trim().toString());
+        String forMatString =
+                inv.getSkuId().toString() + "" + (inv.getLocationId() == null ? "" : inv.getLocationId().toString()) + "" + (inv.getOuterContainerId() == null ? "" : inv.getOuterContainerId().toString()) + ""
+                        + (inv.getInsideContainerId() == null ? "" : inv.getInsideContainerId().toString()) + inv.getCustomerId().toString() + inv.getStoreId().toString() + inv.getInvStatus().toString() + ""
+                        + (inv.getInvType() == null ? "" : inv.getInvType().trim().toString()) + "" + (inv.getBatchNumber() == null ? "" : inv.getBatchNumber().trim().toString()) + ""
+                        + (inv.getMfgDate() == null ? "" : new SimpleDateFormat("yyyyMMddHHmmss").format(inv.getMfgDate())) + "" + (inv.getExpDate() == null ? "" : new SimpleDateFormat("yyyyMMddHHmmss").format(inv.getExpDate())) + ""
+                        + (inv.getCountryOfOrigin() == null ? "" : inv.getCountryOfOrigin().trim().toString()) + "" + (inv.getInvAttr1() == null ? "" : inv.getInvAttr1().trim().toString()) + ""
+                        + (inv.getInvAttr2() == null ? "" : inv.getInvAttr2().trim().toString()) + "" + (inv.getInvAttr3() == null ? "" : inv.getInvAttr3().trim().toString()) + "" + (inv.getInvAttr4() == null ? "" : inv.getInvAttr4().trim().toString())
+                        + "" + (inv.getInvAttr5() == null ? "" : inv.getInvAttr5().trim().toString()) + "" + (inv.getOutboundboxCode() == null ? "" : inv.getOutboundboxCode());
         uuid = Md5Util.getMd5(forMatString);
         return uuid;
     }
-    
+
     /**
      * 
      * @author lichuan
@@ -45,13 +46,14 @@ public class SkuInventoryUuid {
             return uuid;
         }
         // 拼接库存对应字段值
-        String forMatString = inv.getSkuId().toString() + "" + (inv.getLocationId() == null ? "" : inv.getLocationId().toString()) + "" + (inv.getOuterContainerId() == null ? "" : inv.getOuterContainerId().toString()) + ""
-                + (inv.getInsideContainerId() == null ? "" : inv.getInsideContainerId().toString()) + inv.getCustomerId().toString() + inv.getStoreId().toString() + inv.getInvStatus().toString() + ""
-                + (inv.getInvType() == null ? "" : inv.getInvType().trim().toString()) + "" + (inv.getBatchNumber() == null ? "" : inv.getBatchNumber().trim().toString()) + ""
-                + (inv.getMfgDate() == null ? "" : new SimpleDateFormat("yyyyMMddHHmmss").format(inv.getMfgDate())) + "" + (inv.getExpDate() == null ? "" : new SimpleDateFormat("yyyyMMddHHmmss").format(inv.getExpDate())) + ""
-                + (inv.getCountryOfOrigin() == null ? "" : inv.getCountryOfOrigin().trim().toString()) + "" + (inv.getInvAttr1() == null ? "" : inv.getInvAttr1().trim().toString()) + ""
-                + (inv.getInvAttr2() == null ? "" : inv.getInvAttr2().trim().toString()) + "" + (inv.getInvAttr3() == null ? "" : inv.getInvAttr3().trim().toString()) + "" + (inv.getInvAttr4() == null ? "" : inv.getInvAttr4().trim().toString()) + ""
-                + (inv.getInvAttr5() == null ? "" : inv.getInvAttr5().trim().toString());
+        String forMatString =
+                inv.getSkuId().toString() + "" + (inv.getLocationId() == null ? "" : inv.getLocationId().toString()) + "" + (inv.getOuterContainerId() == null ? "" : inv.getOuterContainerId().toString()) + ""
+                        + (inv.getInsideContainerId() == null ? "" : inv.getInsideContainerId().toString()) + inv.getCustomerId().toString() + inv.getStoreId().toString() + inv.getInvStatus().toString() + ""
+                        + (inv.getInvType() == null ? "" : inv.getInvType().trim().toString()) + "" + (inv.getBatchNumber() == null ? "" : inv.getBatchNumber().trim().toString()) + ""
+                        + (inv.getMfgDate() == null ? "" : new SimpleDateFormat("yyyyMMddHHmmss").format(inv.getMfgDate())) + "" + (inv.getExpDate() == null ? "" : new SimpleDateFormat("yyyyMMddHHmmss").format(inv.getExpDate())) + ""
+                        + (inv.getCountryOfOrigin() == null ? "" : inv.getCountryOfOrigin().trim().toString()) + "" + (inv.getInvAttr1() == null ? "" : inv.getInvAttr1().trim().toString()) + ""
+                        + (inv.getInvAttr2() == null ? "" : inv.getInvAttr2().trim().toString()) + "" + (inv.getInvAttr3() == null ? "" : inv.getInvAttr3().trim().toString()) + "" + (inv.getInvAttr4() == null ? "" : inv.getInvAttr4().trim().toString())
+                        + "" + (inv.getInvAttr5() == null ? "" : inv.getInvAttr5().trim().toString());
         uuid = Md5Util.getMd5(forMatString);
         return uuid;
     }
