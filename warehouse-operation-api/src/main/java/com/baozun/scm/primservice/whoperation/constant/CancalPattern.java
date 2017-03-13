9a2d17b030e4db4c6d5496d044b3fe673f9d86da
ming.tang <ming.tang@baozun.cn>
2017-03-13 16:06:00 +0800

    pda人工上架取消

--- warehouse-operation-api/src/main/java/com/baozun/scm/primservice/whoperation/constant/CancalPattern.java
+++ warehouse-operation-api/src/main/java/com/baozun/scm/primservice/whoperation/constant/CancalPattern.java
@@ -25,18 +25,40 @@ import java.io.Serializable;
 public class CancalPattern implements Serializable {
 
     private static final long serialVersionUID = -7666851291432416397L;
-    
+    /**上架(建议上架和人工上架)*/
     public static final int SKU_CANCEL = 1; // sku取消
+    
     public static final int SCAN_LOCATION_CANCEL = 2; // 扫描库位取消
+    
     public static final int CONTAINER_CACNCEL = 3; //容器取消
     
     public static final int INSIDECONTAINER_CANCEL= 4;//内部容器取消
     
-    
     public static final int TIP_LOCATION_CANCEL = 5; // 提示库位取消
-
     
     public static final int OUTERCONTAINER_CANCEL= 6;//外部容器取消
     
+    public static final int MAN_SPILIT_SN_CANCEL = 7;  //人工上架拆箱上架(sn.残次信息取消)
     
+    public static final int MAN_SPILIT_SKU_DETIAL_CANCEL = 8;  //人工上架拆箱上架(sku库存属性取消)
+    
+    /**拣货流程*/
+    
+    public static final int TIP_CAR_CANCEL = 1; //提示小车取消
+    
+    public static final int SCAN_CAR_CANCEL = 2; //扫描小车取消
+    
+    public static final int TIP_LOC_CANCEL = 3;//提示库位取消
+    
+    public static final int SCAN_LOC_CANCEL = 4;//  扫描库位取消
+    
+    public static final int  TIP_OUTCONTAINER_CANCEL = 5;  //提示外部容器取消
+    
+    public static final int SCAN_OUTCONTAINER_CANCEL = 6;  //扫描外部容器取消
+    
+    public static final int TIP_INSIDECONTAINER_CANCEL = 7;   //提示内部容器
+    
+    public static final int SCAN_INSIDECONTAINER_CANCEL = 8;  //扫描内部容器
+    
+    public static final int SCAN_SKU_SCANCEL = 9;  //sku取消
 }
