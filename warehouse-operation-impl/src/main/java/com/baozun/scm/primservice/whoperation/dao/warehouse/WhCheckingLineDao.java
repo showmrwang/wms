dc4c8b4c1fa9862581356c4e722376c1b51cf09f
mingwei.xie <mingwei.xie@baozun.cn>
2017-03-22 14:21:38 +0800

    复核操作相关类

--- warehouse-operation-impl/src/main/java/com/baozun/scm/primservice/whoperation/dao/warehouse/WhCheckingLineDao.java
+++ warehouse-operation-impl/src/main/java/com/baozun/scm/primservice/whoperation/dao/warehouse/WhCheckingLineDao.java
@@ -1,17 +1,15 @@
 /**
  * Copyright (c) 2013 Baozun All Rights Reserved.
  *
- * This software is the confidential and proprietary information of Baozun.
- * You shall not disclose such Confidential Information and shall use it only in
- * accordance with the terms of the license agreement you entered into
- * with Baozun.
+ * This software is the confidential and proprietary information of Baozun. You shall not disclose
+ * such Confidential Information and shall use it only in accordance with the terms of the license
+ * agreement you entered into with Baozun.
  *
- * BAOZUN MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF THE
- * SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
- * PURPOSE, OR NON-INFRINGEMENT. BAOZUN SHALL NOT BE LIABLE FOR ANY DAMAGES
- * SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING
- * THIS SOFTWARE OR ITS DERIVATIVES.
+ * BAOZUN MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF THE SOFTWARE, EITHER
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE, OR NON-INFRINGEMENT. BAOZUN SHALL NOT BE LIABLE FOR ANY DAMAGES
+ * SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS
+ * DERIVATIVES.
  *
  */
 package com.baozun.scm.primservice.whoperation.dao.warehouse;
@@ -19,36 +17,30 @@ package com.baozun.scm.primservice.whoperation.dao.warehouse;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.ibatis.annotations.Param;
-
 import lark.common.annotation.CommonQuery;
 import lark.common.annotation.QueryPage;
 import lark.common.dao.Page;
 import lark.common.dao.Pagination;
+import lark.common.dao.QueryCondition;
 import lark.common.dao.Sort;
 import lark.orm.dao.supports.BaseDao;
 
-import com.baozun.scm.primservice.whoperation.command.warehouse.WhCheckingLineCommand;
 import com.baozun.scm.primservice.whoperation.model.warehouse.WhCheckingLine;
-import com.baozun.scm.primservice.whoperation.model.warehouse.WhPrintInfo;
+
+public interface WhCheckingLineDao extends BaseDao<WhCheckingLine, Long> {
 
 
+    @QueryPage("findListCountByQueryMap")
+    Pagination<WhCheckingLine> findListByQueryMapWithPage(Page page, Sort[] sorts, Map<String, Object> params);
 
-public interface WhCheckingLineDao extends BaseDao<WhCheckingLine,Long>{
+    @QueryPage("queryCount")
+    Pagination<WhCheckingLine> query(Page page, Sort[] sorts, QueryCondition cond);
 
+    List<WhCheckingLine> query(QueryCondition cond);
 
-	@QueryPage("findListCountByQueryMap")
-	Pagination<WhCheckingLine> findListByQueryMapWithPage(Page page,Sort[] sorts,Map<String, Object> params);
-	
-	@CommonQuery
-	int saveOrUpdate(WhCheckingLine o);
-	
-	/**
-     * 根据复核ID查询复核明细
-     * @param checkingId
-     * @param ouId
-     * @return
-     */
-    List<WhCheckingLineCommand> getCheckingLineByCheckingId(@Param("checkingId") Long checkingId, @Param("ouId") Long ouId);
-	
+    Long queryCount(QueryCondition cond);
+
+    @CommonQuery
+    int saveOrUpdate(WhCheckingLine o);
+
 }
