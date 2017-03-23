0fd075b2488a9ed678ee086d1df3a2ad0dd8edda
qiming.liu <kJd6*NGk>
2017-03-23 20:47:06 +0800

    操作台复核

--- warehouse-operation-api/src/main/java/com/baozun/scm/primservice/whoperation/manager/checking/CheckingManager.java
+++ warehouse-operation-api/src/main/java/com/baozun/scm/primservice/whoperation/manager/checking/CheckingManager.java
@@ -5,4 +5,44 @@ import com.baozun.scm.primservice.whoperation.manager.BaseManager;
 
 public interface CheckingManager extends BaseManager {
     
+    /**
+     * 打印装箱清单
+     * 
+     * @param userId 用户Id
+     * @param ouId 仓库Id
+     */
+    void printPackingList(Long userId, Long ouId);
+    
+    /**
+     * 打印销售清单
+     * 
+     * @param userId 用户Id
+     * @param ouId 仓库Id
+     */
+    void printSalesList(Long userId, Long ouId);
+    
+    /**
+     * 打印面单
+     * 
+     * @param userId 用户Id
+     * @param ouId 仓库Id
+     */
+    void printSinglePlane(Long userId, Long ouId);
+    
+    /**
+     * 打印箱标签
+     * 
+     * @param userId 用户Id
+     * @param ouId 仓库Id
+     */
+    void printBoxLabel(Long userId, Long ouId);
+    
+    /**
+     * 打印发票（复核）
+     * 
+     * @param userId 用户Id
+     * @param ouId 仓库Id
+     */
+    void printInvoiceReview(Long userId, Long ouId);
+    
 }
--- warehouse-operation-impl/src/main/java/com/baozun/scm/primservice/whoperation/manager/checking/CheckingManagerImpl.java
+++ warehouse-operation-impl/src/main/java/com/baozun/scm/primservice/whoperation/manager/checking/CheckingManagerImpl.java
@@ -14,11 +14,18 @@
 
 package com.baozun.scm.primservice.whoperation.manager.checking;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.baozun.scm.baseservice.print.command.PrintDataCommand;
+import com.baozun.scm.baseservice.print.manager.printObject.PrintObjectManagerProxy;
+import com.baozun.scm.primservice.whoperation.constant.Constants;
 import com.baozun.scm.primservice.whoperation.manager.BaseManagerImpl;
 
 @Service("checkingManager")
@@ -26,5 +33,78 @@ import com.baozun.scm.primservice.whoperation.manager.BaseManagerImpl;
 public class CheckingManagerImpl extends BaseManagerImpl implements CheckingManager {
     
     public static final Logger log = LoggerFactory.getLogger(CheckingManagerImpl.class);
+    
+    @Autowired
+    private PrintObjectManagerProxy printObjectManagerProxy;
+
+    @Override
+    public void printPackingList(Long userId, Long ouId) {
+        // 打印装箱清单
+        try {
+            PrintDataCommand printDataCommand = new PrintDataCommand();
+            // 需要明天确认            
+            List<Long> facilityIdsList = Arrays.asList();
+            printDataCommand.setIdList(facilityIdsList);
+            printObjectManagerProxy.printCommonInterface(printDataCommand, Constants.PRINT_ORDER_TYPE_16, userId, ouId);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void printSalesList(Long userId, Long ouId) {
+        // 打印销售清单
+        try {
+            PrintDataCommand printDataCommand = new PrintDataCommand();
+            // 需要明天确认            
+            List<Long> facilityIdsList = Arrays.asList();
+            printDataCommand.setIdList(facilityIdsList);
+            printObjectManagerProxy.printCommonInterface(printDataCommand, Constants.PRINT_ORDER_TYPE_13, userId, ouId);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void printSinglePlane(Long userId, Long ouId) {
+        // 打印面单
+        try {
+            PrintDataCommand printDataCommand = new PrintDataCommand();
+            // 需要明天确认            
+            List<Long> facilityIdsList = Arrays.asList();
+            printDataCommand.setIdList(facilityIdsList);
+            printObjectManagerProxy.printCommonInterface(printDataCommand, Constants.PRINT_ORDER_TYPE_15, userId, ouId);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void printBoxLabel(Long userId, Long ouId) {
+        // 打印箱标签
+        try {
+            PrintDataCommand printDataCommand = new PrintDataCommand();
+            // 需要明天确认            
+            List<Long> facilityIdsList = Arrays.asList();
+            printDataCommand.setIdList(facilityIdsList);
+            printObjectManagerProxy.printCommonInterface(printDataCommand, Constants.PRINT_ORDER_TYPE_1, userId, ouId);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void printInvoiceReview(Long userId, Long ouId) {
+        // 打印发票（复核）
+        try {
+            PrintDataCommand printDataCommand = new PrintDataCommand();
+            // 需要明天确认            
+            List<Long> facilityIdsList = Arrays.asList();
+            printDataCommand.setIdList(facilityIdsList);
+            printObjectManagerProxy.printCommonInterface(printDataCommand, Constants.PRINT_ORDER_TYPE_14, userId, ouId);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
