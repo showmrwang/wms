43e32f84b51d8dc53bdf06a5868c5c29b78a76bf
hui.xu <hui.xu@baozun.cn>
2017-04-12 11:38:33 +0800

    dubbo配置

--- warehouse-operation-impl/src/main/java/com/baozun/scm/primservice/whoperation/manager/rule/WhHandoverStationRecommendManagerImpl.java
+++ warehouse-operation-impl/src/main/java/com/baozun/scm/primservice/whoperation/manager/rule/WhHandoverStationRecommendManagerImpl.java
@@ -1,16 +1,16 @@
 /**
  * Copyright (c) 2013 Baozun All Rights Reserved.
- *
+ * 
  * This software is the confidential and proprietary information of Baozun. You shall not disclose
  * such Confidential Information and shall use it only in accordance with the terms of the license
  * agreement you entered into with Baozun.
- *
+ * 
  * BAOZUN MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF THE SOFTWARE, EITHER
  * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE, OR NON-INFRINGEMENT. BAOZUN SHALL NOT BE LIABLE FOR ANY DAMAGES
  * SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS
  * DERIVATIVES.
- *
+ * 
  */
 package com.baozun.scm.primservice.whoperation.manager.rule;
 
@@ -48,9 +48,9 @@ import com.baozun.scm.primservice.whoperation.model.warehouse.WhHandoverStation;
 
 /**
  * @author lichuan
- *
+ * 
  */
-@Service("whHandoverStationRecommandManager")
+@Service("whHandoverStationRecommendManager")
 @Transactional
 public class WhHandoverStationRecommendManagerImpl extends BaseManagerImpl implements WhHandoverStationRecommendManager {
     protected static final Logger log = LoggerFactory.getLogger(WhHandoverStationRecommendManagerImpl.class);
