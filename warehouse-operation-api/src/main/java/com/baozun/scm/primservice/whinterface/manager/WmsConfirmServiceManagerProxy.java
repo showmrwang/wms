12bdd8603045ee405117d73293e4ca49736b8da0
bin.hu <bin.hu@baozun.com>
2017-04-13 12:23:01 +0800

    全部客户同步接口

--- warehouse-operation-api/src/main/java/com/baozun/scm/primservice/whinterface/manager/WmsConfirmServiceManagerProxy.java
+++ warehouse-operation-api/src/main/java/com/baozun/scm/primservice/whinterface/manager/WmsConfirmServiceManagerProxy.java
@@ -14,14 +14,15 @@ import com.baozun.scm.primservice.whinterface.model.outbound.WmsOutboundConfirm;
 public interface WmsConfirmServiceManagerProxy {
 
     /**
-     * 同步库存流失 bin.hu
+     * 同步库存流水 bin.hu
      * 
      * @param beginTime not null 数据开始时间
      * @param endTime not null 数据结束时间
      * @param whCode not null 仓库编码
+     * @param customerCode not null 客户编码
      * @return
      */
-    List<WmsSkuInventoryFlow> wmsSkuInventoryFlow(Date beginTime, Date endTime, String whCode);
+    List<WmsSkuInventoryFlow> wmsSkuInventoryFlow(Date beginTime, Date endTime, String whCode, String customerCode);
 
     /**
      * 同步出库单状态反馈 bin.hu
