1d857042db425d49200fff95cd16c09f3a3ea882
chuan.li <chuan.li@baozun.com>
2017-04-27 16:31:02 +0800

    出库单头和明细状态

--- warehouse-operation-api/src/main/java/com/baozun/scm/primservice/whoperation/constant/OdoStatus.java
+++ warehouse-operation-api/src/main/java/com/baozun/scm/primservice/whoperation/constant/OdoStatus.java
@@ -8,6 +8,34 @@ public class OdoStatus implements Serializable {
      * 
      */
     private static final long serialVersionUID = 249822883185178906L;
+    
+    /**
+     * 出库单头和明细状态
+     * 
+     * @author lichuan
+     */
+    public static final String CREATING = "0"; // 新建中、待创建
+    public static final String NEW = "1"; // 新建
+    public static final String MERGE = "5"; // 已合并
+    public static final String WAVE = "8"; // 波次中
+    public static final String WAVE_FINISH = "10"; // 波次完成
+    public static final String RELEASE_WORK = "20"; // 工作已释放
+    public static final String PICKING = "25"; // 拣货中
+    public static final String PICKING_FINISH = "30";// 拣货完成
+    public static final String COLLECTION = "35";// 集货中
+    public static final String COLLECTION_FINISH = "40"; // 集货完成
+    public static final String SEEDING = "45"; // 播种中
+    public static final String SEEDING_FINISH = "50"; // 播种完成
+    public static final String CHECKING = "55"; // 复核中
+    public static final String CHECKING_FINISH = "60";// 复核完成
+    public static final String WEIGHING = "65"; // 称重中
+    public static final String WEIGHING_FINISH = "70"; // 称重完成
+    public static final String HANDOVER = "75";// 交接中
+    public static final String HANDOVER_FINISH = "80"; // 交接完成
+    public static final String CANCEL = "88"; // 取消
+    public static final String PARTLY_FINISH = "99";// 部分完成
+    public static final String FINSH = "100"; // 完成、关闭
+    
 
     /** ODO */
     public static final String ODO_NEW = "1";// 新建
