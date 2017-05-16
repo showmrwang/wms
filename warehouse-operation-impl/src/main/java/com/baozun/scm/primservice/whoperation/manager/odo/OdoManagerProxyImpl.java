184675f59b3119a5319e182d2c8409425c542e7d
kai.zhu <kai.zhu1@baozun.com>
2017-05-16 17:09:26 +0800

    异常修改

--- warehouse-operation-impl/src/main/java/com/baozun/scm/primservice/whoperation/manager/odo/OdoManagerProxyImpl.java
+++ warehouse-operation-impl/src/main/java/com/baozun/scm/primservice/whoperation/manager/odo/OdoManagerProxyImpl.java
@@ -2759,7 +2759,7 @@ public class OdoManagerProxyImpl implements OdoManagerProxy {
                 String packageCenterName = res.getPackageCenterName(); // 集包地名称
                 transMgmt.setTransBigWord(transBigWord);
                 transMgmt.setTmsCode(tmsCode);
-                transMgmt.setLogisticsCode(logisticsCode.toUpperCase());
+                transMgmt.setLogisticsCode(null == logisticsCode ? null : logisticsCode.toUpperCase());
                 transMgmt.setPackageCenterCode(packageCenterCode);
                 transMgmt.setPackageCenterName(packageCenterName);
                 WhOdodeliveryInfo delivery = new WhOdodeliveryInfo();
