27f11ea5283f45858da4fcd55f8b6676d9e241f2
hui.xu <hui.xu@baozun.com>
2017-06-03 14:57:55 +0800

    集货插入运输服务商和时效类型  交接异常处理

--- warehouse-operation-impl/src/main/java/com/baozun/scm/primservice/whoperation/manager/handover/WhOutboundDeliveryConfirmManagerImpl.java
+++ warehouse-operation-impl/src/main/java/com/baozun/scm/primservice/whoperation/manager/handover/WhOutboundDeliveryConfirmManagerImpl.java
@@ -9,7 +9,6 @@ import java.util.Map;
 
 import lark.common.annotation.MoreDB;
 
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -70,7 +69,7 @@ public class WhOutboundDeliveryConfirmManagerImpl extends BaseManagerImpl implem
             paramOrderConfirmContent.setExttransorderid(whOutboundDeliveryConfirm.getExtTransOrderId());
             paramOrderConfirmContent.setWhcode(whOutboundDeliveryConfirm.getOuCode());
             paramOrderConfirmContent.setOwnercode(whOutboundDeliveryConfirm.getStoreCode());
-            paramOrderConfirmContent.setLpcode(StringUtils.upperCase(whOutboundDeliveryConfirm.getLogisticsCode()));
+            paramOrderConfirmContent.setLpcode(whOutboundDeliveryConfirm.getLogisticsCode().toUpperCase());
             paramOrderConfirmContent.setTrackingno(whOutboundDeliveryConfirm.getWaybillCode());
             // paramOrderConfirmContent.setChildtracknolis();
 
