0a247f88246179a222ec6afe35d34470abcb1db4
yimin.lu <lymyiyefeng@baozun.cn>
2017-06-07 20:01:41 +0800

    库内移动：库存日志添加

--- warehouse-operation-impl/src/main/java/com/baozun/scm/primservice/whoperation/manager/odo/wave/CreateInWarehouseMoveWorkManagerImpl.java
+++ warehouse-operation-impl/src/main/java/com/baozun/scm/primservice/whoperation/manager/odo/wave/CreateInWarehouseMoveWorkManagerImpl.java
@@ -30,6 +30,7 @@ import com.baozun.scm.primservice.whoperation.command.warehouse.inventory.WhSkuI
 import com.baozun.scm.primservice.whoperation.constant.CacheConstants;
 import com.baozun.scm.primservice.whoperation.constant.Constants;
 import com.baozun.scm.primservice.whoperation.constant.DbDataSource;
+import com.baozun.scm.primservice.whoperation.constant.InvTransactionType;
 import com.baozun.scm.primservice.whoperation.constant.OperationStatus;
 import com.baozun.scm.primservice.whoperation.constant.WorkStatus;
 import com.baozun.scm.primservice.whoperation.dao.odo.WhOdoDao;
@@ -156,7 +157,7 @@ public class CreateInWarehouseMoveWorkManagerImpl extends BaseManagerImpl implem
                 isSuccess = true;
             }    
         } catch (Exception e) {
-            log.error("CreateInWarehouseMoveWorkManagerImpl createAndExecuteInWarehouseMoveWork error" + e);
+            log.error("CreateInWarehouseMoveWorkManagerImpl createAndExecuteInWarehouseMoveWork error", e);
             throw new BusinessException(ErrorCodes.SYSTEM_EXCEPTION);
         }
         return isSuccess;
@@ -321,7 +322,7 @@ public class CreateInWarehouseMoveWorkManagerImpl extends BaseManagerImpl implem
      */
     private String saveInWarehouseMoveWork(WhSkuInventoryAllocatedCommand skuInventoryAllocatedCommand, Long ouId, Long userId) {
         //获取工作类型      
-        WorkType workType = this.workTypeDao.findWorkTypeByworkCategory("IN_WAREHOUSE_MOVE", skuInventoryAllocatedCommand.getOuId());
+        WorkType workType = this.workTypeDao.findWorkTypeByworkCategory(Constants.WORKCATEGORY_IN_WAREHOUSE_MOVE, skuInventoryAllocatedCommand.getOuId());
         //调编码生成器工作头实体标识        
         String workCode = codeManager.generateCode(Constants.WMS, Constants.WHWORK_MODEL_URL, "", "WORK", null);
         //封装数据        
@@ -935,7 +936,7 @@ public class CreateInWarehouseMoveWorkManagerImpl extends BaseManagerImpl implem
     @MoreDB(DbDataSource.MOREDB_SHARDSOURCE)
     public Boolean executeInWarehouseMoveWork(String inWarehouseMoveWorkCode, Long ouId, Long userId, String snKey) {
         try {
-            Warehouse warehouse = warehouseManager.findWarehouseById(ouId);
+            Warehouse warehouse = warehouseManager.findWarehouseByIdExt(ouId);
             if (null == warehouse) {
                 throw new BusinessException(ErrorCodes.PARAMS_ERROR);
             }
@@ -968,7 +969,6 @@ public class CreateInWarehouseMoveWorkManagerImpl extends BaseManagerImpl implem
                 //根据uuid获取库存
                 WhSkuInventory skuInventory = new WhSkuInventory();
                 skuInventory.setUuid(operationLineCommand.getUuid());
-                skuInventory.setIsLocked(null);
                 List<WhSkuInventory> skuInventoryLst = skuInventoryDao.findWhSkuInventoryByPramas(skuInventory);
                 List<WhSkuInventorySnCommand> whSkuInventorySnCommandLst = new ArrayList<WhSkuInventorySnCommand>();
                 whSkuInventorySnCommandLst = whSkuInventorySnDao.findWhSkuInventoryByUuid(operationLineCommand.getOuId(), operationLineCommand.getUuid());
@@ -976,13 +976,16 @@ public class CreateInWarehouseMoveWorkManagerImpl extends BaseManagerImpl implem
                 for(WhSkuInventory whSkuInventory : skuInventoryLst){
                     if(null == whSkuInventory.getOccupationCode() && 0 != allocatedQty.compareTo(0.00)){
                         if(allocatedQty < whSkuInventory.getOnHandQty()){
+                            Double onHandQty = whSkuInventory.getOnHandQty();
                             whSkuInventory.setOnHandQty(whSkuInventory.getOnHandQty() - allocatedQty); 
                             skuInventoryDao.saveOrUpdateByVersion(whSkuInventory);
                             insertGlobalLog(GLOBAL_LOG_UPDATE, whSkuInventory, whSkuInventory.getOuId(), userId, null, null);
+                            this.insertSkuInventoryLog(whSkuInventory.getId(), whSkuInventory.getOnHandQty(), onHandQty, warehouse.getIsTabbInvTotal(), whSkuInventory.getOuId(), userId, InvTransactionType.INTRA_WH_MOVE);
                             break;
                         }else{
                             allocatedQty = allocatedQty - whSkuInventory.getOnHandQty();
                             skuInventoryDao.deleteWhSkuInventoryById(whSkuInventory.getId(), whSkuInventory.getOuId());
+                            this.insertSkuInventoryLog(whSkuInventory.getId(), Constants.DEFAULT_DOUBLE, whSkuInventory.getOnHandQty(), warehouse.getIsTabbInvTotal(), whSkuInventory.getOuId(), userId, InvTransactionType.INTRA_WH_MOVE);
                             insertGlobalLog(GLOBAL_LOG_DELETE, whSkuInventory, whSkuInventory.getOuId(), userId, null, null);
                         }
                     }
@@ -1002,6 +1005,7 @@ public class CreateInWarehouseMoveWorkManagerImpl extends BaseManagerImpl implem
                 whSkuInventory.setOnHandQty(skuInventoryTobefilledLst.get(0).getQty());
                 whSkuInventory.setFrozenQty(0.00);
                 skuInventoryDao.insert(whSkuInventory);
+                this.insertSkuInventoryLog(whSkuInventory.getId(), whSkuInventory.getOnHandQty(), Constants.DEFAULT_DOUBLE, warehouse.getIsTabbInvTotal(), whSkuInventory.getOuId(), userId, InvTransactionType.INTRA_WH_MOVE);
                 insertGlobalLog(GLOBAL_LOG_INSERT, whSkuInventory, whSkuInventory.getOuId(), userId, null, null);
                 if(null != whSkuInventorySnCommandLst && 0 < whSkuInventorySnCommandLst.size()){
                     Integer  snCount = 0;
